9147510ca4d1980f3cbdae26d65c4197c3590b97
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-02-05 20:02:39 +0100

    Basic sidebar structure

---
+++ Interface/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/styles.css">
+    <title>ToDo - App</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="sidebar">
+            <div class="header">
+                <ion-icon name="image-outline" size = "large"></ion-icon> 
+                <h4>Welcome User!</h4> 
+            </div>
+            
+            <div class="list">
+                <ul>
+                    <div class="counter">
+                    <li><a href="#">Tasks</a></li> <span>1</span>
+                    </div>
+                    <li><a href="#">Upcoming</a></li>
+                    <li><a href="#">Projects</a></li>
+                </ul>
+            </div>
+            <div class="footer">
+                <div class="setting">
+                    <ion-icon name="settings-outline" size = "medium"></ion-icon>
+                    <h5>Settings</h5>
+                </div>
+                <div class="logout">
+                    <ion-icon name="log-out-outline" size = "medium"></ion-icon>
+                    <h5>logout</h5> 
+                </div>
+            </div>
+        </div> 
+    </div>
+    <div class="main">
+
+    </div>
+    
+
+
+
+
+
+
+
+
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
