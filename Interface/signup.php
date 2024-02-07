2a07d9beb03a2fdbf450cd1b67600cba3605a8a8
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-02-07 11:17:38 +0100

    Add sign up form for user registration

---
+++ Interface/signup.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/signup.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="right">
+            <p>Ready to Start</p>
+            <span class="span">Organizing</span>
+            <p >Your</p>
+            <p id="para">Day-To-Day </p><span class="span"> Tasks</span>
+
+            <div >
+                <form  class="form">
+                    <input type="text" name="fullname" id="" placeholder="Enter your Fullname">
+                    <input type="email" name="email" id="" placeholder="Enter your Email">
+                    <input type="password" name="password" id="" placeholder="Password">
+                    <input type="password" name="password" id="" placeholder="Reapeat da Password">
+                    <button type="submit">Sign Up</button>
+                </form>
+            </div>
+        </div>
+        <div class="left">
+            <img src="../assets/pic3.jpeg" alt="">
+        </div>
+    </div>
+</body>
+</html>
