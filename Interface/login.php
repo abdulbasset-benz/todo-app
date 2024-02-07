423de5219e3654920b2b4b6647038ed3c634191e
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-02-07 22:04:55 +0100

    Structure login page

---
+++ Interface/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/login.css">
+    <title>Login - To Do</title>
+</head>
+<body>
+<div class="wrapper">
+        <div class="left">
+            <img src="../assets/flowers.png" alt="">
+        </div>
+        <div class="right">
+            
+            <form class="form">
+                
+                <div class="welcome">
+                    <h4>Welcome</h4>
+                    <span>Back!</span>
+                </div>
+                
+                <input type="text" name="" id="" placeholder="username or email">
+                <input type="password" name="" id="" placeholder="password">
+                <input type="submit" value="LOGIN">
+            </form>
+        </div>
+        
+    </div>
+</body>
+</html>
