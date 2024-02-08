c216968878e144544bd20eaf82f36dddbef71e30
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-02-08 17:24:45 +0100

    Connect to database

--- Logic/dbcon.php
+++ Logic/dbcon.php
@@ -1,2 +1,15 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "todo";
 
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Connexion réussie
+    echo "Connexion à la base de données Todo réussie ya kho !";
+  } catch (PDOException $e) {
+    // Erreur de connexion
+    echo "Échec de la connexion : " . $e->getMessage();
+  }
+  
