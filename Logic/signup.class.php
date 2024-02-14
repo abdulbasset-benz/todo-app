a8350b1b3856fec2d59aa6495edf930d1b4cdd72
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-02-14 20:59:21 +0100

    Create user class and fn to validate name and email

--- Logic/signup.class.php
+++ Logic/signup.class.php
@@ -1,3 +1,32 @@
 <?php
-require_once('dbcon.php');
+    require_once('dbcon.php');
 
+    class user{
+        private $fullname;
+        private $email;
+        private $password;
+        private $password_repeat;
+
+        public function __construct($fullname, $email, $password, $password_repeat)
+        {
+            $this->fullname = $fullname;
+            $this->email = $email;
+            $this->password = $password;
+            $this->password_repeat = $password_repeat;
+        }
+
+        function validateName($fullname){
+            if(empty($name)){
+                return "name is required!";
+            }
+            return "";
+        }
+
+        function validateEmail($email){
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                return "valid email is required!";
+            }
+            return "";
+        }
+    }
+?>
