d040ea769802a71ce61a8f4baff9d8c4ed77cce0
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-02-21 20:51:49 +0100

    Finish user registration method

--- Logic/signup.class.php
+++ Logic/signup.class.php
@@ -12,14 +12,39 @@
         public function registerUser($fullname, $email, $password, $password_repeat){
             $user = new user($fullname, $email, $password, $password_repeat);
 
+            // validate user information
             $errors = $user->validate();
 
+            // hash the password for more security
+            $hash = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
+
+            // error handling
             if(!empty($errors)){
                 return $errors;
             }
 
+            // check if user exists inside the database
             if($this->userExists($fullname, $email)){
                 return "User already exists";
+            }
+            
+            // if it doesn't exist , execute this query
+
+            try {
+                $stmt = $this->db->connect()->prepare("INSERT INTO users (Username, Email, PasswordHash) VALUES (:fullname, :email, :hash)");
+
+                $stmt->bindParam(':fullname', $fullname);
+                $stmt->bindParam(':email', $email);
+                $stmt->bindParam(':hash', $hash);
+
+                if($stmt->execute()){
+                    return "User registered successfully";
+                }else{
+                    return "Error registering user";
+                }
+
+            }catch (PDOException $e){
+                return "Error: " . $e->getMessage();
             }
         }
 
