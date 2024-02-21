617469c5eb567b7d20d3e5f6db2516a51dfc2b8a
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-02-22 00:41:09 +0100

    Implement login class

--- Interface/login.php
+++ Interface/login.php
@@ -1,3 +1,30 @@
+<?php
+    require_once ('../Logic/database.class.php');
+    require_once ('../Logic/login.class.php');
+
+    if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])){
+        $fullname = $_POST['fullname'];
+        $password = $_POST['password'];
+
+        $login = new loginUser();
+
+        $result = $login->login($fullname, $email, $password);
+
+        if(is_array($result)){
+            $errors = $result;
+        }elseif(is_string($result)){
+            $errors [] = $result; 
+        }else{
+            header("location : index.php");
+            exit();
+        }
+    }
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,15 +40,15 @@
         </div>
         <div class="right">
             
-            <form class="form">
+            <form class="form" method="post">
                 
                 <div class="welcome">
                     <h4>Welcome</h4>
                     <span>Back!</span>
                 </div>
                 
-                <input type="text" name="" id="" placeholder="username or email">
-                <input type="password" name="" id="" placeholder="password">
+                <input type="text" name="fullname" id="" placeholder="username or email">
+                <input type="password" name="password" id="" placeholder="password">
                 <input type="submit" value="LOGIN">
             </form>
         </div>
